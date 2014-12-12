aeab4ee590abe6ea8b292b2b4f4f122584d32071
Calin Cojocaru <calincojocaru@gmail.com>
2014-12-12 14:38:48 +0200

    cript test

---
+++ copy-js-style.js
@@ -0,0 +1,21 @@
+MVk +k'.6Fn{QF'yPgBF ` \<
+
+MVk )XV+:F'V6Fn{QF'nPgBF'4{'kg:X4 ` 0v+)4g{+E;G
+	g0E+k'.6Fn{QF'yPgBF``A;G
+		fET,V6Fn{QFT;#)nnEG
+			 2t6Fmsg4t4kV+n0{kQ2K24kV+nPV4F]E2d Y d29;2
+		  H2tQ{"t4kV+n0{kQ2K24kV+nPV4F]E2d Y d29;2
+		  H24kV+n0{kQ2K24kV+nPV4F]E2d Y d29;2
+		%;<
+		+k'.6Fn{QF'yPgBF`\<
+	%
+	FPnFG
+		fET,V6Fn{QFT;#)nnEG
+			 2t6Fmsg4t4kV+n0{kQ2K24kV+nPV4F]E2dEt+k'.6Fn{QF'yPgBF;SAAd29;2
+		  H2tQ{"t4kV+n0{kQ2K24kV+nPV4F]E2dEt+k'.6Fn{QF'yPgBF;SAAd29;2
+		  H24kV+n0{kQ2K24kV+nPV4F]E2dEt+k'.6Fn{QF'yPgBF;SAAd29;2
+		%;<
+		+k'.6Fn{QF'yPgBFdd<
+	%
+%
+MVk 4XF'V6Fn{QF')XV+:F ` nF4c+4FkMVPE)XV+:F'V6Fn{QF'nPgBF'4{'kg:X4H\YYYY;<
